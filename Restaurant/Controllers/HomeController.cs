1a71f19df24635aa586449e34c3fa71da1074264
Andriy Yahello <andrey.lv.west@gmail.com>
2017-07-14 20:15:52 +0300

    testing model

---
+++ Restaurant/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Restaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            //ViewBag.Location = "Ukraine, Lviv";
+
+            var model = new AboutModel();
+            model.Name = "Andriy";
+            model.Location = "Ukraine, Lviv";
+            //use this model
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
