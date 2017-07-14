a7653ad29d92e57f2f7679719e5315492ea91f7f
Andriy Yahello <andrey.lv.west@gmail.com>
2017-07-14 22:10:51 +0300

    C:/Program Files/Git/loud can give different results

--- Restaurant/Controllers/LoudController.cs
+++ Restaurant/Controllers/LoudController.cs
@@ -16,7 +16,18 @@ namespace Restaurant.Controllers
 
             //return View(message);
             //to display a message from /Loud/{name}
-            return Content(message);
+            //return Content(message);
+
+            //redirecting to home if the path doesn't exist
+            //return RedirectToAction("Index", "Home", new { name = name });
+
+            //
+            //return RedirectToRoute("Default", new { controlle = "Home", action = "About" });
+            //prints the content of owner.txt if we go to /loud
+            //return File(Server.MapPath("~/Content/Owner.txt"), "text/text");
+
+            //return Json
+            return Json(new { Message = message, Name = "Robert" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
