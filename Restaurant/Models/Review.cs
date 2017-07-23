02b8f78b17286cd54637a8d7f62cb65eb9a8512d
Andriy Yahello <andrey.lv.west@gmail.com>
2017-07-23 16:50:03 +0300

    added validation for rating and body

--- Restaurant/Models/Review.cs
+++ Restaurant/Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -13,8 +14,20 @@ namespace Restaurant.Models
         //public string Name { get; set; }
         //public string City { get; set; }
         //public string Country { get; set; }
+        //rating gange from 1 to 5
+        [Range(1,5)]
+        [Required]
         public float Rating { get; set; }
+
+        [Required]
+        //leng of body is less then 900 characters
+        [StringLength(900)]
+        //and update db  update-database -Verbose -force
         public string Body { get; set; }
+
+        [Display(Name = "User Name")]
+        //if a user forfot to specipy a name
+        [DisplayFormat(NullDisplayText = "anonymous")]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
     }
